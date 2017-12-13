7046607cafe17687996e30967e606d2c0d3cdc06
Michael Horojanski <mic3ael@gmail.com>
2017-12-13 15:15:06 +0200

    push

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'q1w2e3'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 PORT = 5000
